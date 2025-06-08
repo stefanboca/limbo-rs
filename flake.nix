e46e104876bd9e699c3afb48cfc7880d9bdd4877
Gabe Dunn <gabe@gabedunn.dev>
2025-06-08 13:20:02 -0600

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Limbo bar, now with more rust";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, lib, system, ... }: {
+        # use fenix overlay
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ inputs.fenix.overlays.default ];
+        };
+
+        packages = {
+          limbo-rs = let
+            inherit (inputs'.fenix.packages.minimal) toolchain;
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = toolchain;
+              rustc = toolchain;
+            };
+          in rustPlatform.buildRustPackage {
+            pname = "limbo-rs";
+            version = "0-unstable";
+
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            meta = {
+              description = "Limbo bar, now with more rust";
+              homepage = "https://github.com/co-conspirators/limbo-rs";
+              license = lib.licenses.asl20;
+              platforms = lib.platforms.linux;
+              mainProgram = "limbo";
+            };
+          };
+
+          default = self'.packages.limbo-rs;
+        };
+
+        devShells.default = pkgs.mkShell {
+          shellHook = let
+            libs = with pkgs; [ wayland libxkbcommon vulkan-loader libGL ];
+            libPaths = lib.makeLibraryPath libs;
+          in ''
+            export LD_LIBRARY_PATH="${libPaths}:$LD_LIBRARY_PATH"
+          '';
+        };
+      };
+    };
+}
